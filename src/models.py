e047579aa496a22e6b1564be1d7db543d5f5ba3c
Pipecontreras02 <pipecontreras2@gmail.com>
2022-03-21 03:45:36 +0000

    data modeling uploaded!

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import  Table, Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,94 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+association_table = Table('user_favorites', Base.metadata,
+    Column('user_id', ForeignKey('user.id')),
+    Column('character_id', ForeignKey('character.id')),
+    Column('planet_id', ForeignKey('planet.id')),
+    Column('starship_id', ForeignKey('starship.id'))
+)
+
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    character = relationship("Character",
+                    secondary=association_table)
+    starship = relationship("Starship",
+                    secondary=association_table)
+    planet = relationship("Planet",
+                    secondary=association_table)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+
+class Starship(Base):
+    __tablename__ = 'starship'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    model = Column(String(250))
+    manufacturer = Column(String(250))
+    cost_in_credits = Column(Integer)
+    length = Column(Integer)
+    max_atmosphering_speed = Column(Integer)
+    crew = Column(String(250))
+    passengers = Column(Integer)
+    cargo_capacity = Column(Integer)
+    consumables = Column(String(250))
+    hyperdrive_rating = Column(Integer)
+    mglt = Column(Integer)
+    starship_class = Column(String(250))
+    pilots = Column(String(250))
+    films = Column(String(250))
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    diameter = Column(Integer)
+    climate = Column(String(250))
+    gravity = Column(Integer)
+    terrain = Column(String(250))
+    surface_water = Column(Integer)
+    population = Column(Integer)
+    residents = Column(String(250))
+    films = Column(String(250))
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Character(Base):
+    __tablename__ = 'character'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    height = Column(String(20))
+    mass = Column(Integer)
+    hair_color = Column(String(250))
+    skin_color = Column(String(250))
+    eye_color = Column(String(250))
+    birth_year = Column(String(250))
+    gender = Column(String(25))
+    homeworld = Column(String(250))
+    films = Column(String(250))
+    species = Column(String(250))
+    vehicles = Column(String(250))
+    starships = Column(String(250))
+    
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     def to_dict(self):
         return {}
